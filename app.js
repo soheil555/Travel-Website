9a1333ea74d2befec9a84b129d05eaabe5afd218
soheil rasekh <soheilrasekh555@gmail.com>
2020-12-24 20:05:45 +0330

    add slide animation

--- app.js
+++ app.js
@@ -1,26 +1,63 @@
 let controller;
 let slideScene;
+let pageScene;
 
 function animateSlides(){
+
      controller = new ScrollMagic.Controller();
 
-     let sliders = document.querySelectorAll(".slide");
-     let nav = document.querySelector(".nav-header");
+     const slides = document.querySelectorAll(".slide");
+     const nav = document.querySelector(".nav-header");
 
-     sliders.forEach(slide =>{
+     slides.forEach((slide,index,slides) => {
 
-            let revealImg = slide.querySelector(".reveal-img");
-            let img = slide.querySelector("img");
-            let revealText = slide.querySelector(".reveal-text");
+          const img = slide.querySelector("img");
+          const revealImg = slide.querySelector(".reveal-img");
+          const revealText = slide.querySelector(".reveal-text");
+
+          const slideTl = gsap.timeline({defaults:{duration:1,ease:"power.inOut"}});
+          slideTl.fromTo(nav,{y:"-100%"},{y:"0%"});
+          slideTl.fromTo(revealImg,{x:"0%"},{x:"100%"},"-=1");
+          slideTl.fromTo(revealText,{x:"0%"},{x:"100%"},'-=0.75');
+          slideTl.fromTo(img,{scale:1.5},{scale:1},"-=1");
 
 
-            let slideTl = gsap.timeline({defaults:{duration:1,ease:'power2.inOut'}});
+          slideScene = new ScrollMagic.Scene({
+               triggerElement:slide,
+               triggerHook:0.25,
+               reverse:false
+          })
+          .setTween(slideTl)
+          .addIndicators({
+               colorStart:'white',
+               colorTrigger:'white',
+               name:'slide'
+          })
+          .addTo(controller);
 
-            slideTl.fromTo(revealImg,{x:'0%'},{x:'100%'});
-            slideTl.fromTo(img,{scale:2},{scale:1},'-=1');
-            slideTl.fromTo(revealText,{x:'0%'},{x:'100%'},'-=0.75');
-            slideTl.fromTo(nav,{y:"-100%"},{y:'0%'},'-=0.5');
+          const pageTl = gsap.timeline();
+          let nextSlide = slides.length-1 == index ? 'last':slides[index+1];
+          pageTl.fromTo(nextSlide,{y:"0%"},{y:"50%"});
+          pageTl.fromTo(slide,{opacity:1,scale:1},{opacity:0,scale:0.5})
+          pageTl.fromTo(nextSlide,{y:"50%"},{y:"0%"},'-=0.5');
+          pageScene = new ScrollMagic.Scene({
+               triggerElement:slide,
+               duration:'100%',
+               triggerHook:0
+          })
+          .setPin(slide,{pushFollowers:false})
+          .setTween(pageTl)
+          .addIndicators({
+               colorStart:'white',
+               colorTrigger:'white',
+               name:'page',
+               indent:200
+          })
+          .addTo(controller)
+
+
      });
+
 }
 
 animateSlides();
